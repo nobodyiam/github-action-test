0287818c956f7bab3c9508393b8f3f8f0b8035b1
Jason Song <songshun.ss@antfin.com>
2020-03-29 16:49:26 +0800

    add minus

--- src/main/java/com/nobodyiam/test/Demo.java
+++ src/main/java/com/nobodyiam/test/Demo.java
@@ -2,6 +2,10 @@ package com.nobodyiam.test;
 
 public class Demo {
 
+  public int minus(int number1, int number2) {
+    return number1 - number2;
+  }
+
   public int add(int number1, int number2) {
     return number1 + number2;
   }
