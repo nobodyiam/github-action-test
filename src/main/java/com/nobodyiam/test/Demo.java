8545dc48917a5d9a451a8854a6c53fa935504deb
Jason Song <songshun.ss@antfin.com>
2020-04-06 16:51:45 +0800

    add multiply

--- src/main/java/com/nobodyiam/test/Demo.java
+++ src/main/java/com/nobodyiam/test/Demo.java
@@ -9,4 +9,8 @@ public class Demo {
   public int add(int number1, int number2) {
     return number1 + number2;
   }
+
+  public int multiply(int number1, int number2) {
+    return number1 * number2;
+  }
 }
